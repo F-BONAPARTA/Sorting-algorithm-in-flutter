2ce6fcb7df33c08629679aac7fb249a1bb2ee51e
F-BONAPARTA <67749770+F-BONAPARTA@users.noreply.github.com>
2020-10-08 04:49:16 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -36,24 +36,6 @@ class _HomeState extends State<Home> {
     Item(85.0, Colors.blue),
     Item(80.0, Colors.blue),
   ];
-  List<double> list = [
-    13.0,
-    33.0,
-    5.0,
-    56.0,
-    20.0,
-    65.0,
-    50.0,
-    16.0,
-    70.0,
-    60.0,
-    25.0,
-    30.0,
-    40.0,
-    35.0,
-    85.0,
-    80.0
-  ];
 
   @override
   Widget build(BuildContext context) {
